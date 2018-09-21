a60b78531d6d17008df001de67b7a2e460c36b26
Kaleb Kougl <jisakk5@MacBook135.local>
2018-09-21 13:18:23 -0500

    Initial commit, adding reused components, setup data for redux

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common/Header';
+
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>            
+            <View>
+                <Header headerText="TechStack" />
+            </View>
+        </Provider>
+    );
+};
+
+export default App;
